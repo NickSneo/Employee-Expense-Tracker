f717bc1eb4749b9bc655caf861cbcb4138250518
AbhayAravinda <33093450+AbhayAravinda@users.noreply.github.com>
2020-10-15 18:00:34 +0530

    Add files via upload

--- EmployeeExpenseTracker/urls.py
+++ EmployeeExpenseTracker/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from Register.views import loginUser, home, logoutUser, inputExpense, registerEmp
-from ViewExpenses.views import viewExpenses
+from ViewExpenses.views import viewExpenses, trackByEmp, trackByTime, trackByTag
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,5 +11,8 @@ urlpatterns = [
     path('registerEmp/', registerEmp, name="registerEmp"),
     path('inputExpense/', inputExpense, name="inputExpense"),
     path('viewExpenses/', viewExpenses, name="viewExpenses"),
+    path('trackByEmp/',trackByEmp,name='trackByEmp'),
+    path('trackByTime/',trackByTime,name='trackByTime'),
+    path('trackByTag/',trackByTag,name='trackByTag'),
 
 ]
