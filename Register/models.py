8bb0e9e8526c81ffe6c77fb7876f4e53e04fce04
AbhayAravinda <33093450+AbhayAravinda@users.noreply.github.com>
2020-10-15 18:01:13 +0530

    Add files via upload

--- Register/models.py
+++ Register/models.py
@@ -9,9 +9,14 @@ class Employee(models.Model):
     Dept = models.CharField(blank=False, max_length=50)
     Salary = models.IntegerField(blank=False)
 
+    def __str__(self):
+    	return("Employee Name: " + str(self.EmpName) + ", Employee Id: " + str(self.EmpId))
 
 class Expenses(models.Model):
     Time = models.DateTimeField(auto_now_add=True)
     EmpId = models.ForeignKey(Employee, on_delete=models.CASCADE)
     Tags = models.CharField(blank=False, max_length=50)
     Amount = models.IntegerField(blank=True)
+
+    def __str__(self):
+    	return("Employee Id: " + str(self.EmpId) + ", Amount: " + str(self.Amount))
