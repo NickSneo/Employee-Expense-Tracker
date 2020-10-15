f01e67a9e1729d36bdb815421c9051e1593306b2
NickSneo <17ucc040@lnmiit.ac.in>
2020-10-16 01:27:19 +0530

    viewExpenses added pie charts

--- EmployeeExpenseTracker/urls.py
+++ EmployeeExpenseTracker/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from Register.views import loginUser, home, logoutUser, inputExpense, registerEmp
-from ViewExpenses.views import viewExpenses, trackByEmp, trackByTime, trackByTag
+from ViewExpenses.views import viewExpenses, trackByEmp, trackByTime, trackByTag, trackByDept
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,8 +11,9 @@ urlpatterns = [
     path('registerEmp/', registerEmp, name="registerEmp"),
     path('inputExpense/', inputExpense, name="inputExpense"),
     path('viewExpenses/', viewExpenses, name="viewExpenses"),
-    path('trackByEmp/',trackByEmp,name='trackByEmp'),
-    path('trackByTime/',trackByTime,name='trackByTime'),
-    path('trackByTag/',trackByTag,name='trackByTag'),
+    path('trackByEmp/', trackByEmp, name='trackByEmp'),
+    path('trackByTime/', trackByTime, name='trackByTime'),
+    path('trackByTag/', trackByTag, name='trackByTag'),
+    path('trackByDept/', trackByDept, name='trackByDept'),
 
 ]
--- ViewExpenses/views.py
+++ ViewExpenses/views.py
@@ -2,55 +2,82 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from Register.models import Employee, Expenses
+import datetime
+import pytz
 # Create your views here.
+
 
 @login_required(login_url="/loginUser")
 def viewExpenses(request):
     return render(request, "viewExpenses.html")
 
+
 @login_required(login_url="/loginUser")
 def trackByEmp(request):
-	expenses = []
-	sum = 0
-	name = ""
-	salary=""
-	if request.method == 'POST':
-		EmpId = request.POST['EmpId']
-		emp_expenses = Expenses.objects.filter(EmpId=EmpId)
+    expenses = []
+    tagExpense = {}
+    total = 0
+    name = ""
+    salary = ""
+    if request.method == 'POST':
+        EmpId = request.POST['EmpId']
+        emp_expenses = Expenses.objects.filter(EmpId=EmpId)
 
-		name = Employee.objects.filter(EmpId=EmpId)[0].EmpName
-		salary = Employee.objects.filter(EmpId=EmpId)[0].Salary
-		
-		for e in emp_expenses:
-			expenses.append({'time':e.Time, 'tag':e.Tags, 'amount':e.Amount})
-			sum+=e.Amount
-	employees = Employee.objects.filter()
-	empIds = []
-	for e in employees:
-		empIds.append(e.EmpId)
-	return render (request, 'trackByEmp.html',{"empIds": empIds, "expenses":expenses,"sum":sum,"name":name,"salary":salary})
+        employee = Employee.objects.get(EmpId=EmpId)
+        name = employee.EmpName
+        salary = employee.Salary
+        startdate = pytz.utc.localize(
+            datetime.datetime.now() - datetime.timedelta(days=30))
+        for e in emp_expenses:
+            if e.Time >= startdate:
+                expenses.append(
+                    {'time': e.Time, 'tag': e.Tags, 'amount': e.Amount})
+                total += e.Amount
+                tagExpense[e.Tags] = tagExpense.get(e.Tags, 0) + e.Amount
+    employees = Employee.objects.all()
+    empIds = []
+    for e in employees:
+        empIds.append(e.EmpId)
+    return render(request, 'trackByEmp.html', {"empIds": empIds, "expenses": expenses, "total": total, "name": name, "salary": salary, "tagExpense": tagExpense})
+
 
 @login_required(login_url="/loginUser")
 def trackByTime(request):
-	# Code to handle post request
-	return render (request, 'trackByTime.html')
+    # Code to handle post request
+    return render(request, 'trackByTime.html')
+
 
 @login_required(login_url="/loginUser")
 def trackByTag(request):
-	expenses = []
-	sum = 0
-	tag = ""
-	if request.method == 'POST':
-		tag = request.POST['Tag']
-		tag_expenses = Expenses.objects.filter(Tags=tag)
-		
-		for t in tag_expenses:
-			print(t)
-			expenses.append({'time':t.Time, 'id': t.EmpId, 'amount':t.Amount})
-			sum+=t.Amount
+    tag_list = ['Personal', 'Insurance',
+                'Travel', 'Rent', 'Relocation', 'Misc']
 
-	tag_list = []
-	for item in Expenses.objects.filter():
-		if str(item.Tags) not in tag_list:
-			tag_list.append(str(item.Tags))
-	return render (request, 'trackByTag.html',{"tags": tag_list, "expenses":expenses,"sum":sum,"name":tag})
+    tag_expenses = Expenses.objects.all()
+    tagExpense = {}
+    startdate = pytz.utc.localize(
+        datetime.datetime.now() - datetime.timedelta(days=30))
+    for t in tag_expenses:
+        if t.Time >= startdate:
+            tagExpense[t.Tags] = tagExpense.get(t.Tags, 0) + t.Amount
+    if request.method == 'POST':
+        expenses = []
+        tag = request.POST['Tag']
+        for t in tag_expenses:
+            if t.Time >= startdate:
+                if t.Tags == tag:
+                    expenses.append(
+                        {'time': t.Time, 'id': t.EmpId.EmpId, 'amount': t.Amount})
+                tagExpense[t.Tags] = tagExpense.get(t.Tags, 0) + t.Amount
+        if tag in tagExpense:
+            total = tagExpense[tag]
+        else:
+            total = 0
+
+        return render(request, 'trackByTag.html', {"tags": tag_list, "expenses": expenses, "sum": total, "name": tag, "tagExpense": tagExpense})
+    else:
+        return render(request, 'trackByTag.html', {"tags": tag_list, "tagExpense": tagExpense})
+
+
+@login_required(login_url="/loginUser")
+def trackByDept(request):
+    return render(request, 'trackByDept.html')
